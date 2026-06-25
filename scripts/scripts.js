4634ba7c7e015e02ffab05dffcbc59745766b208
fkakatie <ksexton@adobe.com>
2026-06-25 03:08:28 -0600

    fix: header/footer specificity

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -269,7 +269,7 @@ async function loadEager(doc) {
  * @param {Element} doc The container element
  */
 async function loadLazy(doc) {
-  loadHeader(doc.querySelector('header'));
+  loadHeader(doc.querySelector('main > header'));
 
   const main = doc.querySelector('main');
   await loadSections(main);
@@ -278,7 +278,7 @@ async function loadLazy(doc) {
   const element = hash ? doc.getElementById(hash.substring(1)) : false;
   if (hash && element) element.scrollIntoView();
 
-  loadFooter(doc.querySelector('footer'));
+  loadFooter(doc.querySelector('main > footer'));
 
   loadCSS(`${window.hlx.codeBasePath}/styles/lazy-styles.css`);
   loadFonts();
